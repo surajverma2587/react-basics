4978ed95fbdb83672318a0a311f1053f7c5bdc1c
Suraj Verma <suraj.verma@bbc.co.uk>
2018-12-27 13:55:18 +0000

    code

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-/* import styles here */
+/* import PropTypes and styles here */
 
 const MenuItem = (/* props object */) => {
-  /* bind the props object data to your html template data requirement */
-  /* return the populated html template */
+  /* bind the data from the props object to the html template */
+  /* validate the props and render the component IF valid ELSE null */
+  /* replace the bottom return statement with the populated html template */
+  return (
+    <div>
+      MenuItem Component
+    </div>
+  );
 };
 
 /* display name for the component */
 MenuItem.displayName = 'MenuItem';
 
 MenuItem.propTypes = {
-  /* add your Proptypes here */
+  /* add Proptypes here */
+};
+
+MenuItem.defaultProps = {
+  /* add default values to required props */
 };
 
 export default MenuItem;
