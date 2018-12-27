d15cccb420c5db289d89e05c52db9743cc971e00
Suraj Verma <suraj.verma@bbc.co.uk>
2018-12-27 14:35:29 +0000

    code

--- src/components/Menu/MenuSection.js
+++ src/components/Menu/MenuSection.js
@@ -1,14 +1,26 @@
 import React from 'react';
-/* import the child components and styles here */
+/* import PropTypes, child components and styles here */
 
 const renderMenuItems = (/* menuItems */) => {
-  // return list of MenuItem components
+  // map through the menuItems and return list of MenuItem components
 };
 
 const MenuSection = (/* props object */) => {
-  /* bind the props object data to your html template data requirement */
-  /* remember to map through each menu item and return the MenuItem component */
-  /* return the populated html template */
+  /* bind the data from the props object to the html template */
+  /* validate the props and render the component IF valid ELSE null */
+  /* remember to call renderMenuItems() to render the list of MenuItem components */
+  /* replace the bottom return statement with the populated html template */
+};
+
+/* display name for the component */
+MenuSection.displayName = 'MenuSection';
+
+MenuSection.propTypes = {
+  /* add Proptypes here */
+};
+
+MenuSection.defaultProps = {
+  /* add default values to required props */
 };
 
 export default MenuSection;
